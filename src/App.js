ead87a0a9dcfe3022b827d5b40e1b28c5e0947f0
Esrakgun <akgundogduesr@gmail.com>
2025-02-09 21:21:05 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+import  './PokeCard.css';
+import Pokegame from "./PokeGame";
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Pokemon Go Card Game</h1>
+     <Pokegame />
+    
+    </div>
+  );
+}
+
+export default App;
